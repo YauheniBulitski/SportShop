1878f11b8892c17c2330c5098d0965ab519a6421
YauheniBulitski <yauheni.bulitski@gmail.com>
2021-04-15 14:27:42 +0300

    fix internalization

--- web/src/main/java/root/validator/ProductValidation.java
+++ web/src/main/java/root/validator/ProductValidation.java
@@ -30,43 +30,43 @@ public class ProductValidation implements CustomValidator<ProductDto> {
     public void check(ProductDto productDto, Errors errors) {
 
         if (productDto.getName().length() == 0 || "".equals(productDto.getName().trim())) {
-            errors.rejectValue("name", "10", "Название не может быть пустым");
+            errors.rejectValue("name", "10", "err.product_name_empty");
         } else if (productDto.getName().length() < 3 && productDto.getName().length() > 256) {
-            errors.rejectValue("name", "11", "Название должно содержать не меньше 3 и не больше 256 символов");
+            errors.rejectValue("name", "11", "err.product_name_length");
         }
 
         if (productDto.getPrice() == null) {
-            errors.rejectValue("price", "12", "Поле цена не может быть пустым");
+            errors.rejectValue("price", "12", "err.price_empty");
         } else if (productDto.getPrice() <= 0.0) {
-            errors.rejectValue("price", "13", "Цена не может быть ровна '0' или быть отрицательной");
+            errors.rejectValue("price", "13", "err.price_valid");
         }
 
         if (productDto.getDescribe().length() == 0 || "".equals(productDto.getName().trim())) {
-            errors.rejectValue("describe", "15", "Описание не может быть пустым");
+            errors.rejectValue("describe", "15", "err.desc_empty");
         } else if (productDto.getDescribe().length() < 3 && productDto.getDescribe().length() > 560) {
-            errors.rejectValue("describe", "16", "Название должно содержать не меньше 3 и не больше 560 символов");
+            errors.rejectValue("describe", "16", "err.desc_length");
         }
 
         if (productDto.getCount() == null) {
-            errors.rejectValue("count", "17", "Поле количество не может быть пустым");
+            errors.rejectValue("count", "17", "err.count_empty");
         } else if (productDto.getPrice() <= 0) {
-            errors.rejectValue("count", "18", "Колличество не может быть ровна '0' или быть отрицательным");
+            errors.rejectValue("count", "18", "err.count_valid");
         }
 
         if (productDto.getMaker_id() == null) {
-            errors.rejectValue("maker_id", "20", "Поле производитель не может быть пустым");
+            errors.rejectValue("maker_id", "20", "err.maker_empty");
         } else if (productDto.getMaker_id() <= 0) {
-            errors.rejectValue("maker_id", "21", "Id производителя не может быть ровна '0' или быть отрицательным");
+            errors.rejectValue("maker_id", "21", "err.maker_id_valid");
         } else if (!makerService.existsById(productDto.getMaker_id())) {
-            errors.rejectValue("maker_id", "23", "Производителя с данным Id не существует");
+            errors.rejectValue("maker_id", "23", "err.maker_exist");
         }
 
         if (productDto.getType_id() == null) {
-            errors.rejectValue("type_id", "24", "Поле тип не может быть пустым");
+            errors.rejectValue("type_id", "24", "err.type_empty");
         } else if (productDto.getType_id() <= 0) {
-            errors.rejectValue("type_id", "25", "Id типа не может быть ровна '0' или быть отрицательным");
+            errors.rejectValue("type_id", "25", "err.type_id_valid");
         } else if (!typeService.existsById(productDto.getType_id())) {
-            errors.rejectValue("type_id", "27", "Тип с данным Id не существует");
+            errors.rejectValue("type_id", "27", "err.type_exist");
         }
     }
 
--- web/src/main/java/root/validator/UserValidator.java
+++ web/src/main/java/root/validator/UserValidator.java
@@ -27,37 +27,37 @@ public class UserValidator implements CustomValidator<UserDto> {
     public void check(UserDto userDto, Errors errors) {
 
         if (userDto.getUserName().length() == 0 || "".equals(userDto.getUserName().trim())) {
-            errors.rejectValue("userName", "79", "Имя не может быть пустым");
+            errors.rejectValue("userName", "79", "err.name_empty");
         } else if (userDto.getUserName().length() < 3 || userDto.getUserName().length() > 64) {
-            errors.rejectValue("userName", "80", "Имя должно содержать не меньше 3 и не больше 64 символов");
+            errors.rejectValue("userName", "80", "err.name_contains");
         } else if (!userDto.getUserName().matches("[a-zA-Zа-яА-Я]+")) {
-            errors.rejectValue("userName", "81", "Используйте кириллицу или латиницу");
+            errors.rejectValue("userName", "81", "err.name_letters");
         }
 
         if (userDto.getPhoneNumber().length() == 0 || "".equals(userDto.getPhoneNumber().trim())) {
-            errors.rejectValue("phoneNumber", "89", "Поле не может быть пустым");
+            errors.rejectValue("phoneNumber", "89", "err.phone_empty");
         } else if (!userDto.getPhoneNumber().matches(value)) {
-            errors.rejectValue("phoneNumber", "90", "Введенный номер телефона не соответсвует требуемому формату");
+            errors.rejectValue("phoneNumber", "90", "err.phone_no_format");
         }
 
         if (userDto.getLoginName().length() == 0 || "".equals(userDto.getLoginName().trim())) {
-            errors.rejectValue("loginName", "100", "Логин не может быть пустым");
+            errors.rejectValue("loginName", "100", "err.login.name_empty");
         } else if (userServiceImpl.findByName(userDto.getLoginName()).orElse(null) != null) {
-            errors.rejectValue("loginName", "101", "Пользователь с таким логином уже существует");
+            errors.rejectValue("loginName", "101", "err.login.name_busy");
         } else if (!userDto.getLoginName().matches("[0-9a-zA-Z]+")) {
-            errors.rejectValue("loginName", "81", "Используйте кириллицу и цифры");
+            errors.rejectValue("loginName", "81", "err.login.name_letters");
         } else if (userDto.getLoginName() == null || "".equals(userDto.getLoginName().trim())) {
-            errors.rejectValue("loginName", "102", "Поле не может быть пустым");
+            errors.rejectValue("loginName", "102", "err.login.name_empty");
         } else if (userDto.getLoginName().length() < 3 || userDto.getLoginName().length() > 64) {
-            errors.rejectValue("loginName", "103", "Логин должен содержать не меньше 3 и не больше 64 символов");
+            errors.rejectValue("loginName", "103", "err.login.name_length");
         }
 
         if (userDto.getPassword().length() == 0 || "".equals(userDto.getPassword().trim())) {
-            errors.rejectValue("password", "103", "Пароль не может быть пустым");
+            errors.rejectValue("password", "103", "err.pass_empty");
         } else if (userDto.getPassword().length() < 4 || userDto.getPassword().length() > 80) {
-            errors.rejectValue("password", "104", "Пароль должен содержать не меньше 4 и не больше 80 символов");
+            errors.rejectValue("password", "104", "err.pass_length");
         } else if (!userDto.getPassword().matches("[0-9]+")) {
-            errors.rejectValue("password", "110", "Пароль должен содержать только цифры");
+            errors.rejectValue("password", "110", "err.pass_contains");
         }
     }
 
